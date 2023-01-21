bf542de2c307f43ff64901013b433035403967f5
Sbarrachina <sandrabarrachina1978@gmail.com>
2023-01-21 13:57:38 +0100

    terminado galeria de fotos con funciones javascript,actualizada al menu de proyectos

--- FOTOS/index.js
+++ FOTOS/index.js
@@ -31,4 +31,81 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
+
+}
+
+var closeBtn = document.getElementById("close-modal");
+closeBtn.ontouchstart = closeModal;
+closeBtn.onclick = closeModal;
+
+
+// galeria 3
+
+var filterButtons = document.querySelectorAll(".filter");
+var imgBoxes = document.querySelectorAll(".img-box-3");
+
+// Agregar un evento de click a cada botón de filtro
+filterButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    var category = button.getAttribute("data-category");
+    filterImages(category);
+  });
+});
+
+function filterImages(category) {
+  imgBoxes.forEach(function(imgBox) {
+    if (imgBox.getAttribute("data-category") === category || category === "all") {
+      imgBox.style.display = "block";
+    } else {
+      imgBox.style.display = "none";
+    }
+  });
+}
+
+// // efecto modal. clic en fotos 
+
+var images = document.querySelectorAll('.img-box-3 img');
+var modal = document.getElementById('modal');
+var modalImg = document.getElementById('modal-img');
+var close = document.getElementById('close');
+
+images.forEach(function(image) {
+  image.addEventListener('click', function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  });
+});
+
+close.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+
+// next y preview
+
+var currentSlide = 0;
+var images = document.querySelectorAll('.img-box-3 img');
+var modal = document.getElementById('modal');
+var modalImg = document.getElementById('modal-img');
+var close = document.getElementById('close');
+
+images.forEach(function(image, index) {
+  image.addEventListener('click', function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    currentSlide = index;
+  });
+});
+
+close.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+
+function plusSlides3(n) {
+  currentSlide += n;
+  if (currentSlide < 0) {
+    currentSlide = images.length - 1;
+  } else if (currentSlide >= images.length) {
+    currentSlide = 0;
+  }
+  modalImg.src = images[currentSlide].src;
 }
